f85c8a87eb3eec760353347865632bc5f5d18591
Marcin Pytlak <marcin.pytlak@o2.pl>
2020-12-02 19:41:13 +0100

    add functions in both files js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = 'kamień';
+printMessage ('zagrałem '+ computerMove + '! Jeśli Twój ruch to papier, to wygrywasz');
+
+let playerMove = 'papier';
+printMessage ('zagrałem ' + playerMove + '! Jeżeli Twój tuch to nożyce, to wygrywasz');
