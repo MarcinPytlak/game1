814cd35e56649f4d0cdd0579881791c742756b94
Marcin Pytlak <marcin.pytlak@o2.pl>
2020-12-03 18:09:21 +0100

    Add computer moves

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-let computerMove = 'kamień';
-printMessage ('zagrałem '+ computerMove + '! Jeśli Twój ruch to papier, to wygrywasz');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let playerMove = 'papier';
-printMessage ('zagrałem ' + playerMove + '! Jeżeli Twój tuch to nożyce, to wygrywasz');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+    computerMove = "kamień";
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else if (randomNumber == 3) {
+    computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);
