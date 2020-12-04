d3cb0ac9173ab9105d2ad2426ce8249f2943009d
Marcin Pytlak <marcin.pytlak@o2.pl>
2020-12-04 19:31:17 +0100

    Change js for functions

--- js/script.js
+++ js/script.js
@@ -1,48 +1,50 @@
+function getMoveName(MoveID){
+    if(MoveID == 1){
+        return 'kamień';
+    }else if(MoveID == 2){
+        return 'papier';
+    }else if(MoveID == 3){
+        return 'nożyce';
+    }else{
+        printMessage('Nie znam ruchu o id ' + MoveID +'.');
+    return ('nieznany ruch');
+    }
+}
+function displayResult(computerMove, playerMove){
+    console.log('moves: ', computerMove, playerMove)
+    printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
+
+if( computerMove === playerMove){
+    printMessage('Mamy remis!');
+    console.log('wykonała się 1 zależność, czyli remis ')
+}else if(computerMove =='kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrałeś! Spróbuj raz jeszcze oszukać skrypt :)');
+}else if(computerMove =='kamień' && playerMove =='papier'){
+    printMessage('Gratuluje! Wygrałeś');
+}else if(computerMove =='papier' && playerMove == 'kamień'){
+    printMessage('Przegrałeś! Spróbuj raz jeszcze oszukać skrypt :)');
+}else if (computerMove =='papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}else if(computerMove =='nożyce' && playerMove == 'papier'){
+    printMessage('Przegrałeś! Spróbuj raz jeszcze oszukać skrypt :)');
+}else if (computerMove =='nożyce' && playerMove == 'kamień'){
+    printMessagen('Ty wygrywasz!');
+}else{
+    printMessage('uhuhu! Próbowałeś oszukać Freda, teraz Fred oszuka Ciebie :D');
+}
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
-if (randomNumber == 1) {
-    computerMove = "kamień";
-} else if (randomNumber == 2) {
-    computerMove = 'papier';
-} else if (randomNumber == 3) {
-    computerMove = 'nożyce';
-}
-printMessage('Mój ruch to: ' + computerMove);
+let computerMove = getMoveName(randomNumber);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
-    playerMove = 'kamień';
-}else if (playerInput == '2'){
-    playerMove = 'papier';
-}else if (playerInput == '3'){
-    playerMove = 'nożyce';
-}
-printMessage('Twój ruch to: ' + playerMove)
-
-if( playerMove == '1' || playerMove == '2' || playerMove == '3'){
-}else if( computerMove === playerMove){
-    printMessage('Mamy remis!');
-}else if(computerMove =='kamień' && playerMove == 'nożyce'){
-    printMessage('Przgrałeś! Spróbuj raz jeszcze oszukać skrypt :)');
-}else if(computerMove =='kamień' && playerMove =='papier'){
-    printMessage('Gratuluje! Wygrałeś');
-}else if(computerMove =='papier' && playerMove == 'kamień'){
-    printMessage('Przgrałeś! Spróbuj raz jeszcze oszukać skrypt :)');
-}else if (computerMove =='papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-}else if(computerMove =='nożyce' && playerMove == 'papier'){
-    printMessage('Przgrałeś! Spróbuj raz jeszcze oszukać skrypt :)');
-}else if (computerMove =='nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
-}else{
-    printMessage('uhuhu! Próbowałeś oszukać Freda, teraz Fred oszuka Ciebie :D');
-} 
+displayResult(computerMove, playerMove);
+console.log(getMoveName('2'));
